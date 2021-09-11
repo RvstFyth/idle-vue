8d5e66c0c95e65ae4a82d57c67928d9667c654a6
RvStFyth <r.vansintfeyth@protonmail.com>
2021-09-11 13:33:00 +0200

    Display which axe is used in wc skill page

--- src/views/skills/Woodcutting.vue
+++ src/views/skills/Woodcutting.vue
@@ -2,6 +2,7 @@
   <div class="woodcutting">
     <h1>Woodcutting ({{skill.level}})</h1>
     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+    <h5 style="text-align: left">Axe: {{axesMapping[axe]}}</h5>
     <table class="table table-hover">
       <thead>
       <tr>
@@ -28,13 +29,15 @@ export default {
   data() {
     return {
       skill: this.$store.state.character.data.skills.woodcutting,
+      axe: this.$store.state.character.data.gear.axe,
       resources: {
         log: { name: 'log', level: 1, xp: 25, axe: 1},
         oak: { name: 'oak log', level: 15, xp: 37.5, axe: 1},
         willow: { name: 'willow log', level: 30, xp: 67.5, axe: 1},
         maple: { name: 'maple log', level: 45, xp: 100, axe: 1},
         yew: { name: 'yes log', level: 60, xp: 175, axe: 1},
-      }
+      },
+      axesMapping: ['none', 'bronze', 'iron', 'steel', 'mithril', 'adamant', 'rune']
     }
   },
   methods: {
