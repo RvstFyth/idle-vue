ab1f0cfa3e08aa7d6162fbe6d5326318cccadf50
RvStFyth <r.vansintfeyth@protonmail.com>
2021-10-02 00:16:01 +0200

    Implemented shop buy

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -5,15 +5,17 @@
       <thead>
       <tr>
         <th>Name</th>
+        <th>Price</th>
         <th>Options</th>
       </tr>
       </thead>
       <tbody>
       <tr v-for="(value, name) in shop" v-bind:key="name">
         <td>{{value.name | capitalize}}</td>
+        <td>{{value.price}}</td>
         <td>
-          <input type="number" step="1" :id="name" value="1"> &nbsp;&nbsp;
-          <button class="btn btn-secondary">Buy</button> &nbsp;&nbsp;
+          <input type="number" step="1" :id="value.name" value="1"> &nbsp;&nbsp;
+          <button class="btn btn-secondary" v-on:click="buy(value.name)">Buy</button> &nbsp;&nbsp;
         </td>
       </tr>
       </tbody>
@@ -32,10 +34,21 @@ export default {
     }
   },
   methods: {
-    buy(index) {
-      const item = this.shop[index];
+    buy(i) {
+      const item = this.shop.filter(e => e.name === i)[0];
       if(item) {
-        // TODO
+        const amount = parseInt(document.getElementById(item.name).value);
+        const price = parseInt(amount) * item.price;
+        if (this.$store.state.character.data.gold < price) {
+          this.$alertify.error(`You don't have enough gold..`);
+        }
+        else {
+          this.$store.state.character.data.gold -= price;
+          this.$store.commit('bank/add', {
+            item: item.name, amount: amount
+          });
+          this.$alertify.succes(`You bough ${amount} x` ${item.name})
+        }
       }
     }
   }
