3d2f39fd088645ca2537f97fc89886c405d3be80
RvStFyth <r.vansintfeyth@protonmail.com>
2021-11-07 09:33:37 +0100

    removed the dummy text from the start page, and added some links to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>About</h1>
+    <p>
+      This game is developed by <a href="https://github.com/RvstFyth/" target="_blank">RvStFyth</a>.<br>
+      You can find the source code <a href="https://github.com/RvstFyth/idle-vue/" target="_blank">Here</a>.
+    </p>
   </div>
 </template>
--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="hello">
-    <h1>Start</h1>
+    <h1>Troxa idle</h1>
     <p>
-      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
+      Start your adventure by creating a character!
     </p>
     <form>
-      <div class="row g-3 align-items-center">
-        <div class="col-auto"><label for="characterName" class="form-label">Character name</label></div>
-        <div class="col-auto"><input type="text" id="characterName" class="form-control" v-model="data.newCharacterName"></div>
+<!--      <div class="row g-3 align-items-center">-->
+        <div class="col-auto"><input type="text" id="characterName" class="form-control" v-model="data.newCharacterName" placeholder="character name"></div><br>
         <div class="col-auto"><button type="button" class="btn btn-success" v-on:click="createCharacter">Start!</button> </div>
-      </div>
+<!--      </div>-->
     </form>
   </div>
 </template>
