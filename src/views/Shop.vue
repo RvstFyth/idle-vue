b4c0a13e317d2a4d727dd836dae2d2d4efb4032f
RvStFyth <r.vansintfeyth@protonmail.com>
2021-11-07 09:41:57 +0100

    Fixed the shop buy notification

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -29,7 +29,7 @@ export default {
     return {
       shop: [
         {name: 'tinderbox', price: 250},
-        {name: 'small fishing net', price: 500}
+        {name: 'small fishing net', price: 1}
       ],
     }
   },
@@ -47,7 +47,7 @@ export default {
           this.$store.commit('bank/add', {
             item: item.name, amount: amount
           });
-          this.$alertify.succes(`You bough ${amount} x ${item.name}`);
+          this.$alertify.success(`You bought ${amount} x ${item.name}`);
         }
       }
     }
